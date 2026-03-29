4b390c5563d9dae4f7753b35573b821fe32975fd
riddhisharma7905 <riddhisharma7905@gmail.com>
2026-03-30 00:07:59 +0530

    New Features added

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -5,7 +5,6 @@ import {
   markAllRead,
   markOneRead,
   streamNotifications,
-  clearNotifications,
 } from "../controllers/notificationController.js";
 import { authMiddleware, authMiddlewareQuery } from "../middleware/authMiddleware.js";
 
@@ -19,6 +18,5 @@ router.get("/unread-count", authMiddleware, getUnreadCount);
 router.get("/", authMiddleware, getNotifications);
 router.put("/mark-all-read", authMiddleware, markAllRead);
 router.put("/:id/read", authMiddleware, markOneRead);
-router.delete("/clear-all", authMiddleware, clearNotifications); // <--- Using more explicit name
 
 export default router;
